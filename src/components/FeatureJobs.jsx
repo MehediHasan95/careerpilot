4737bfe1e4fdb16442c18e7f1e621b05a54f70eb
MehediHasan95 <imehedihasan95@gmail.com>
2023-05-10 13:40:17 +0600

    all jobs loaded, design and see more complete

--- src/components/FeatureJobs.jsx
+++ src/components/FeatureJobs.jsx
@@ -3,12 +3,17 @@ import Job from "./Job";
 
 const FeatureJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [limit, setLimit] = useState(4);
 
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
       .then((data) => setJobs(data));
-  }, []);
+  }, [limit]);
+
+  const handleSeeMore = () => {
+    setLimit(jobs.length);
+  };
 
   return (
     <section className="px-2 lg:px-32 mt-24 border">
@@ -21,10 +26,21 @@ const FeatureJobs = () => {
       </div>
       <div>
         <div className="grid gap-5 lg:grid-cols-2">
-          {jobs.map((job) => (
+          {jobs.slice(0, limit).map((job) => (
             <Job key={job.id} job={job} />
           ))}
         </div>
+
+        {jobs.slice(0, limit).length === 4 && (
+          <div className="my-8 flex justify-center">
+            <button
+              onClick={handleSeeMore}
+              className="bg-gradient-to-r from-customBlue to-customPurple text-white py-2 px-4 rounded-md"
+            >
+              See All Jobs
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,16 +1,36 @@
+import { faLocationDot, faSackDollar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 /* eslint-disable react/prop-types */
 const Job = ({ job }) => {
-  console.log(job);
+  //   console.log(job);
   const { company, title, image, location, salary, remote, full_time } = job;
   return (
     <div className="border rounded-md p-5">
       <img src={image} alt={company} className="w-16 mb-5" />
       <h1 className="text-xl font-bold">{title}</h1>
       <p>{company}</p>
-      <p>
-        {location} {salary}
+
+      <div className="my-3">
+        <button className="border border-customBlue text-customBlue font-semibold py-1 px-5 rounded-md me-1">
+          {remote ? "Remote" : "Onsite"}
+        </button>
+        <button className="border border-customBlue text-customBlue font-semibold py-1 px-5 rounded-md ms-1">
+          {full_time ? "Full Time" : "Part Time"}
+        </button>
+      </div>
+
+      <p className="my-3">
+        <span className="me-3">
+          <FontAwesomeIcon icon={faLocationDot} /> {location}
+        </span>
+        <span className="ms-3">
+          <FontAwesomeIcon icon={faSackDollar} /> {salary}
+        </span>
       </p>
-      <button>View Details</button>
+      <button className="bg-gradient-to-r from-customBlue to-customPurple text-white py-2 px-4 rounded-md">
+        View Details
+      </button>
     </div>
   );
 };
